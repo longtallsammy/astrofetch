73f4dc230ee0124a876c6cf1ae7a6071d0d3cea0
longtallsammy <longtallsammy@github.com>
2024-08-07 11:12:22 +0100

    added check for python version

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,14 @@
 import parsecli
 import main
+import sys
+
+requiredMajor = 3
+requiredMinor = 11
+
+versionMajor = sys.version_info.major
+versionMinor = sys.version_info.minor
+
+if versionMajor < requiredMajor or versionMajor == requiredMajor and versionMinor < requiredMinor:
+    exit("Python version 3.11 or higher required")
 
 main.matchCliArgs(parsecli.args)
